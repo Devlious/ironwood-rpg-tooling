3535176cbe177295beb64cc2924c5731e85a4c3a
Humberto Esparza <humberto.dev96@gmail.com>
2023-01-25 14:48:38 -0600

    Ironwood version update to 0.7.3

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -28,19 +28,8 @@ export default function Sidebar() {
                             Alpha
                         </span>
                         <span className="bg-lime-100 text-lime-800 text-sm font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-lime-200 dark:text-lime-900">
-                            v0.6.15a
+                            v0.7.3
                         </span>
-                        {/*<button type="button"*/}
-                        {/*        className="ml-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 inline-flex h-6 w-6 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800"*/}
-                        {/*        data-collapse-toggle="dropdown-cta" aria-label="Close">*/}
-                        {/*    /!*<span className="sr-only">Close</span>*!/*/}
-                        {/*    <svg aria-hidden="true" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"*/}
-                        {/*         xmlns="http://www.w3.org/2000/svg">*/}
-                        {/*        <path fill-rule="evenodd"*/}
-                        {/*              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"*/}
-                        {/*              clip-rule="evenodd"></path>*/}
-                        {/*    </svg>*/}
-                        {/*</button>*/}
                     </div>
                     <p className="mb-3 text-sm text-gray-900 dark:text-gray-400">
                         This is a community based tooling for Ironwood RPG.
